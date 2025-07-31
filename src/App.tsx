c880e9351387641b82c955102627c546ef6637a5
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-07-31 11:35:55 -0300

    Criando componente de menu com links e ícones

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -13,8 +13,9 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
