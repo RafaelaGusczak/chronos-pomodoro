6b4dea529495da4614e1a919a739b3d6438b1e45
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-08-05 12:08:39 -0300

    Capturando o evento de clique em link com preventDefault para tema dark e light

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,10 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
 
 export function App() {
-  // const [numero, setNumero] = useState(() => {
-  //   console.log('lazy initialization');
-
-  //   return 0;
-  // });
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    // setNumero(prevState => prevState + 1);
-    setNumero(1);
-  }
-
   return (
     <>
-      <Heading>Número: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
       <Container>
         <Logo />
       </Container>
@@ -46,7 +30,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText={numero.toString()}
+              labelText='task:'
               id='meuInput'
               type='text'
               placeholder='Digite algo'
