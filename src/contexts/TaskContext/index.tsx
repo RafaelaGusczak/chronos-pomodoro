006a5653d4e9f5d33ad11371d714b6be2bc3938c
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-08-06 13:43:13 -0300

    Context API - useContext + useState (estado + contexto)

--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 import type { TaskStateModel } from '../../models/TaskStateModel';
 
 const initialState: TaskStateModel = {
@@ -31,13 +31,15 @@ type TaskContextProviderProps = {
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
+  const [state, setState] = useState(initialState);
+
   return (
-    <TaskContext.Provider value={{ ...initialContextValue }}>
+    <TaskContext.Provider value={{ state, setState }}>
       {children}
     </TaskContext.Provider>
   );
 }
 
 export function useTaskContext() {
-  return useContext(TaskContext)
+  return useContext(TaskContext);
 }
