fc861ae45ca74aa754c5f879676bc8363487f789
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-08-06 10:33:53 -0300

    Estado global da aplicação com useState e comunicação via props

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,23 @@
-import { Container } from "../../components/Container";
-import { CountDown } from "../../components/CountDown";
-import { MainForm } from "../../components/MainForm";
-import { MainTemplate } from "../../layout/MainLayouts";
+import { Container } from '../../components/Container';
+import { CountDown } from '../../components/CountDown';
+import { MainForm } from '../../components/MainForm';
+import { MainTemplate } from '../../layout/MainLayouts';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
-export function Home() {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomeProps) {
+  const { state, setState } = props;
+
   return (
     <MainTemplate>
       <Container>
         <CountDown />
       </Container>
-      
+
       <Container>
         <MainForm />
       </Container>
