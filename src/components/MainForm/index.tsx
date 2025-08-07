8bcc2ec051bf6e602e8003b774325138706d168d
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-08-07 08:19:11 -0300

    useRef + Ref para não causar re-renderizações nos componentes

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,14 +2,16 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export function MainForm() {
   const [taskName, setTaskName] = useState('');
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('Deu certo', taskName);
+
+    console.log('Deu certo');
   }
 
   return (
@@ -20,8 +22,7 @@ export function MainForm() {
           id='meuInput'
           type='text'
           placeholder='Digite algo'
-          value={taskName}
-          onChange={e => setTaskName(e.target.value)}
+          ref={taskNameInput}
         />
       </div>
 
