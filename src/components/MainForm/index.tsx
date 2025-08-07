a0967eb34d3eeb589d39a55195df96513cfdd499
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-08-07 09:16:42 -0300

    Validando o formulário e criando uma nova tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,15 +3,50 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef, useState } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  const { setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log('Deu certo');
+    if (taskNameInput.current == null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60; // Converter minutos para segundos
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+        }, 
+        activeTask: newTask, 
+        currentCycle: 1, 
+        secondsRemaining, 
+        formattedSecondsRemaining: '00:00', 
+        tasks: [...prevState.tasks, newTask], 
+      }; // Atualiza o estado com a nova tarefa e o tempo restante
+    });
   }
 
   return (
