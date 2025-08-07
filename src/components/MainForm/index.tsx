7fac43b1c9c87a8d3c5e55fd02b984c6c7c05402
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-08-07 11:30:20 -0300

    Desativando o input se tem task ativa (disabled)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -65,6 +65,7 @@ export function MainForm() {
           type='text'
           placeholder='Digite algo'
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
