9cc10a07899d9d12c58cd0437e08fb05a3df38d5
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-08-08 13:37:22 -0300

    Entendendo o mínimo necessário sobre Web Workers

---
+++ src/workers/timeWorker.js
@@ -0,0 +1,23 @@
+self.onmessage = function (event) {
+  console.log('WORKER recebeu:', event.data);
+
+  switch (event.data) {
+    case 'FAVOR': {
+      self.postMessage('Sim, posso fazer um favor.');
+      break;
+    }
+    case 'FALA_OI': {
+      self.postMessage('OK: OI');
+      break;
+    }
+    case 'FECHAR': {
+      self.postMessage('Tá bom, vou fechar.');
+      self.close(); // Encerra o worker
+      break;
+    }
+    default: {
+      self.postMessage('Não entendi');
+    }
+  }
+};
+                                      
